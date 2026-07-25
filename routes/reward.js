2b7f54140757078a770b51f28c6ce356dee7c8c0
dotrongtai <trongtaikc26@gmail.com>
2026-07-26 01:36:13 +0700

    fix

--- routes/reward.js
+++ routes/reward.js
@@ -10,6 +10,9 @@ router.post('/spin', verifyToken, rewardController.spin);
 // 📜 Lấy danh sách quà chưa nhận
 router.get('/unclaimed', verifyToken, rewardController.getUnclaimedRewards);
 
+// 📜 Lấy toàn bộ lịch sử (claimed + unclaimed)  <-- THÊM DÒNG NÀY VÀO
+router.get('/history', verifyToken, rewardController.getHistory);
+
 // 🎁 Nhận vào game
 router.post('/claim', verifyToken, rewardController.claimReward);
 
